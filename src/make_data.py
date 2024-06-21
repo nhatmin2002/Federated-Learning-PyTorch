49b72289991c47c838edbe0c9becb564e76fd24f
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-06-22 03:57:38 +0700

    Update make_data.py

--- src/make_data.py
+++ src/make_data.py
@@ -170,7 +170,7 @@ if __name__ == "__main__":
     print(train_dataset[0])
     print(test_dataset[0])
     # print(user_groups[0])
-    trainloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    trainloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
     for batch_idx, (data, target) in enumerate(trainloader):
         print(f'Batch {batch_idx + 1}:')
         print('Data shape:', data.shape)
