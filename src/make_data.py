3a7a433bfa8bee850abf81a88fe73796b79ba085
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-06-22 02:11:08 +0700

    Update make_data.py

--- src/make_data.py
+++ src/make_data.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import argparse
 from sklearn.utils import shuffle
-from src.
+import torch
+from torchvision import datasets, transforms
+from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
+from sampling import cifar_iid, cifar_noniid
 def load_data(data_dir):
     # Load feature names
     features = []
