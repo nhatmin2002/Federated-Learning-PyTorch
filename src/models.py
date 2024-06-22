2047815a4b48db0a27493bdf9cda17fd90aa8bd4
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-06-22 07:25:59 +0700

    Update models.py

--- src/models.py
+++ src/models.py
@@ -23,7 +23,7 @@ class SimpleCNN(nn.Module):
 
 class SimpleMLP(nn.Module):
     def __init__(self, dim_in, dim_hidden, dim_out):
-        super(MLP, self).__init__()
+        super(SimpleMLP, self).__init__()
         self.dim_in = dim_in
         self.layer_input = nn.Linear(dim_in, dim_hidden)
         self.relu = nn.ReLU()
