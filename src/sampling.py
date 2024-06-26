7561775d92f8f6b4ecce024918876831e955ab20
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-06-26 23:14:07 +0700

    Update sampling.py

--- src/sampling.py
+++ src/sampling.py
@@ -23,34 +23,34 @@ def mnist_iid(dataset, num_users):
     return dict_users
 
 
-# def mnist_noniid(dataset, num_users):
-#     """
-#     Sample non-I.I.D client data from MNIST dataset
-#     :param dataset:
-#     :param num_users:
-#     :return:
-#     """
-#     # 60,000 training imgs -->  200 imgs/shard X 300 shards
-#     num_shards, num_imgs =50,147
-#     #200, 300
-#     idx_shard = [i for i in range(num_shards)]
-#     dict_users = {i: np.array([]) for i in range(num_users)}
-#     idxs = np.arange(num_shards*num_imgs)
-#     labels = dataset.train_labels.numpy()
+def mnist_noniid(dataset, num_users):
+    """
+    Sample non-I.I.D client data from MNIST dataset
+    :param dataset:
+    :param num_users:
+    :return:
+    """
+    # 60,000 training imgs -->  200 imgs/shard X 300 shards
+    num_shards, num_imgs =50,147
+    #200, 300
+    idx_shard = [i for i in range(num_shards)]
+    dict_users = {i: np.array([]) for i in range(num_users)}
+    idxs = np.arange(num_shards*num_imgs)
+    labels = dataset.train_labels.numpy()
 
-#     # sort labels
-#     idxs_labels = np.vstack((idxs, labels))
-#     idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
-#     idxs = idxs_labels[0, :]
+    # sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+    idxs = idxs_labels[0, :]
 
-#     # divide and assign 2 shards/client
-#     for i in range(num_users):
-#         rand_set = set(np.random.choice(idx_shard, 2, replace=False))
-#         idx_shard = list(set(idx_shard) - rand_set)
-#         for rand in rand_set:
-#             dict_users[i] = np.concatenate(
-#                 (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
-#     return dict_users
+    # divide and assign 2 shards/client
+    for i in range(num_users):
+        rand_set = set(np.random.choice(idx_shard, 2, replace=False))
+        idx_shard = list(set(idx_shard) - rand_set)
+        for rand in rand_set:
+            dict_users[i] = np.concatenate(
+                (dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
+    return dict_users
 
 
 def mnist_noniid_unequal(dataset, num_users):
