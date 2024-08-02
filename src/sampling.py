f264828ea0f4ae9f9bfaa8858da083d4bb1e2d3b
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-08-02 20:28:47 +0700

    Update sampling.py

--- src/sampling.py
+++ src/sampling.py
@@ -32,7 +32,9 @@ def mnist_noniid(dataset, num_users):
     """
     # 60,000 training imgs -->  200 imgs/shard X 300 shards
     #7209 ----> 81x89 shards
-    num_shards, num_imgs =107,77
+    num_shards, num_imgs =8,919
+
+    # num_shards, num_imgs =107,77
     #200, 300
     idx_shard = [i for i in range(num_shards)]
     dict_users = {i: np.array([]) for i in range(num_users)}
